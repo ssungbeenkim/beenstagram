df9b5965063c5b91ffb451ec14449676f7d0df7e
wandakim <nwd1583@naver.com>
2023-08-27 07:29:46 +0900

    chore: Add react-spinners

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,3 +1,4 @@
+import { getUserByUsername } from '@/service/user';
 import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 import { authOptions } from '../auth/[...nextauth]/route';
@@ -5,5 +6,11 @@ import { authOptions } from '../auth/[...nextauth]/route';
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
   const user = session?.user;
-  return NextResponse.json('Hello World!');
+
+  if (!user) {
+    return new Response('Authentication Error', { status: 401 });
+  }
+  return getUserByUsername(user.username).then((data) =>
+    NextResponse.json(data)
+  );
 }
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,6 +1,8 @@
 'use client';
+import { DetailUser } from '@/model/user';
 import useSWR from 'swr';
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR('/api/me');
-  return <p>FollowingBar</p>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me');
+  const users = data?.following;
+  return <section></section>;
 }
