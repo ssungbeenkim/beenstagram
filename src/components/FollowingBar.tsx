0adcf5d68f1dc0446dc9c37c461717b32c2beb3e
wandakim <nwd1583@naver.com>
2023-08-27 08:25:48 +0900

    feat: scrollable bar

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -4,9 +4,14 @@ import Link from 'next/link';
 import { PropagateLoader } from 'react-spinners';
 import useSWR from 'swr';
 import Avatar from './ui/Avatar';
+import ScrollableBar from './ui/ScrollableBar';
 export default function FollowingBar() {
   const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me');
-  const users = data?.following;
+  const users = data?.following && [
+    ...data.following,
+    ...data.following,
+    ...data.following,
+  ];
   return (
     <section className='mb-4 flex min-h-[90px] w-full items-center justify-center overflow-x-auto rounded-lg p-4 shadow-sm shadow-neutral-300'>
       {loading ? (
@@ -15,21 +20,20 @@ export default function FollowingBar() {
         (!users || users.length === 0) && <p>{`Not following anyone`}</p>
       )}
       {users && users.length > 0 && (
-        <ul className='flex w-full gap-2'>
+        <ScrollableBar>
           {users.map(({ image, username }) => (
-            <li key={username}>
-              <Link
-                className='flex w-20 flex-col items-center'
-                href={`/user/${username}`}
-              >
-                <Avatar image={image} highlight />
-                <p className='w-full overflow-hidden text-ellipsis text-center text-sm'>
-                  {username}
-                </p>
-              </Link>
-            </li>
+            <Link
+              key={username}
+              className='flex w-20 flex-col items-center'
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className='w-full overflow-hidden text-ellipsis text-center text-sm'>
+                {username}
+              </p>
+            </Link>
           ))}
-        </ul>
+        </ScrollableBar>
       )}
     </section>
   );
