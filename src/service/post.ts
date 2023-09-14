693ed4203fa4823f69ffd52a7eb8f5617941f763
wandakim <nwd1583@naver.com>
2023-09-14 19:03:07 +0900

    chore: Update next

--- src/service/post.ts
+++ src/service/post.ts
@@ -2,23 +2,25 @@ import { SimplePost } from '@/model/post';
 import { client, urlFor } from './sanity';
 
 const simplePostProjection = `
-...,
-"username": author->username,
-"userImage": author->image,
-"image": photo,
-"likes": likes[]->username,
-"text": comments[0].comment,
-"comments": count(comments),
-"id":_id,
-"createdAt":_createdAt  
+    ...,
+    "username": author->username,
+    "userImage": author->image,
+    "image": photo,
+    "likes": likes[]->username,
+    "text": comments[0].comment,
+    "comments": count(comments),
+    "id":_id,
+    "createdAt":_createdAt
 `;
 
 export async function getFollowingPostsOf(username: string) {
   return client
     .fetch(
-      `*[_type == "post" && author->username == "${username}"|| 
-        author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
-        | order(_createdAt desc){${simplePostProjection}}`
+      `*[_type =="post" && author->username == "${username}"
+          || author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
+          | order(_createdAt desc){
+          ${simplePostProjection}
+        }`
     )
     .then(mapPosts);
 }
@@ -27,38 +29,36 @@ export async function getPost(id: string) {
   return client
     .fetch(
       `*[_type == "post" && _id == "${id}"][0]{
-        ...,
-        "username": author->username,
-        "userImage": author->image,
-        "image": photo,
-        "likes": likes[]->username,
-        comments[]{comment, "username": author->username, "image": author->image},
-        "id":_id,
-        "createdAt":_createdAt
-      }`
+      ...,
+      "username": author->username,
+      "userImage": author->image,
+      "image": photo,
+      "likes": likes[]->username,
+      comments[]{comment, "username": author->username, "image": author->image},
+      "id":_id,
+      "createdAt":_createdAt
+    }`
     )
-    .then((post) => ({
-      ...post,
-      image: urlFor(post.image),
-    }));
+    .then((post) => ({ ...post, image: urlFor(post.image) }));
 }
+
 export async function getPostsOf(username: string) {
   return client
     .fetch(
       `*[_type == "post" && author->username == "${username}"]
-        | order(_createdAt desc){
-          ${simplePostProjection}
-        }`
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
     )
     .then(mapPosts);
 }
 export async function getLikedPostsOf(username: string) {
   return client
     .fetch(
-      `*[_type == "post" && "${username}" in likes[]->username ]
-        | order(_createdAt desc){
-          ${simplePostProjection}
-        }`
+      `*[_type == "post" && "${username}" in likes[]->username]
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
     )
     .then(mapPosts);
 }
@@ -66,9 +66,9 @@ export async function getSavedPostsOf(username: string) {
   return client
     .fetch(
       `*[_type == "post" && _id in *[_type=="user" && username=="${username}"].bookmarks[]._ref]
-        | order(_createdAt desc){
-          ${simplePostProjection}
-        }`
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
     )
     .then(mapPosts);
 }
@@ -94,7 +94,7 @@ export async function likePost(postId: string, userId: string) {
 }
 
 export async function dislikePost(postId: string, userId: string) {
-  return client //
+  return client
     .patch(postId)
     .unset([`likes[_ref=="${userId}"]`])
     .commit();
